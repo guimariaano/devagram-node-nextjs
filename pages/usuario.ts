8b1e0dcef54a38c6d0f8a6a1817e28a3701ad82e
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-05-08 22:47:51 -0300

    editar perfil de usuario

--- pages/usuario.ts
+++ pages/usuario.ts
@@ -3,8 +3,48 @@ import type {RespostaPadraoMsg} from '../types/RespostaPadraoMsg';
 import { validarTokenJWT } from '../middlewares/validarTokenJWT';
 import { conectarMongoDB } from '../middlewares/conectarMongoDB';
 import { UsuarioModel } from '../models/UsuarioModel';
+import nc from 'next-connect';
+import {upload, uploadImagemCosmic} from '../services/uploadImagemCosmic';
 
-const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any >) => {
+const handler = nc()
+.use(upload.single('file'))
+.put(async(req : NextApiRequest, res  : NextApiResponse<RespostaPadraoMsg>) => {
+    try{
+        const {userId} = req?.query;
+        const usuario = await UsuarioModel.findById(userId);
+
+        if(!usuario){
+            return res.status(400).json({erro : 'usuario nao encontado'});
+        }
+
+        const {nome} = req.body;
+        if(nome && nome.length > 2){
+            usuario.nome = nome;
+            
+        }
+
+        const {file} = req;
+        if(file && file.originalname){
+            const image = await uploadImagemCosmic(req);
+            if(image && image.media && image.media.url)
+                usuario.avatares = image.media.url;
+
+        }
+
+        await UsuarioModel
+        .findByIdAndUpdate({_id : usuario._id}, usuario);
+
+        return res.status(200).json({msg : 'usuario alterado com sucesso'});
+       
+
+    }catch(e){
+        console.log(e);
+    }
+    return res.status(400).json({erro : 'nao foi possivel atualizar usuario'});
+
+})
+
+.get(async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any >) => {
 
     try{
         
@@ -20,7 +60,17 @@ const usuarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<Respo
     return res.status(400).json({err0 : 'Nao foi possivel obter dados do usuario'})
 
     
+}); 
+
+export const config = {
+    api : {
+        bodyParser : false
+    }
 }
 
-
-export default validarTokenJWT(conectarMongoDB(usuarioEndpoint));
+declare module 'next' {
+    export interface NextApiRequest {
+      file: any; // Tipo do arquivo que você espera
+    }
+  }
+export default validarTokenJWT(conectarMongoDB(handler));
